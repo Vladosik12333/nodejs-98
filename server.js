60a4270fa9016b35999233def2e541933c17a230
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-05-19 14:55:37 +0300

    Added Error middleware

--- server.js
+++ server.js
@@ -1,33 +1,49 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 const PORT = 3000;
 const app = express();
 
 //
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(cors());
 
 app.use(express.json());
 
-app.get('/p1', (_, res) => {
-  res.send('hello world!!').status(234);
+app.get("/p1", (_, res, next) => {
+  try {
+    throw {};
+    res.send("hello world!!").status(234);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get('/p2', (_, res) => {
-  res.send('hello world!!').status(234);
+app.get("/p2", (_, res, next) => {
+  try {
+    throw {};
+    res.send("hello world!!").status(234);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use((_, res) => {
-  res.status(404).json({ message: 'This route not found' });
+  res.status(404).json({ message: "This route not found" });
 });
 
-app.listen(PORT, error => {
+//Middleware of Error
+app.use((error, req, res, next) => {
+  const { message = "Server internall error 344!", status = 500 } = error;
+  res.status(status).json(message);
+});
+
+app.listen(PORT, (error) => {
   if (error) {
-    console.log('Server crushed. error: ', error);
+    console.log("Server crushed. error: ", error);
     process.exit(1);
   }
-  console.log('Server is running on port: ', PORT);
+  console.log("Server is running on port: ", PORT);
 });
