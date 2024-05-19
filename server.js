3c247a6e1ee1332489e0909b976930a98659007e
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-05-19 14:34:07 +0300

    added middlewares

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-import express from "express";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 const PORT = 3000;
 const app = express();
 
-app.get("/p1", (_, res) => {
-  res.send("hello world!!").status(234);
+//
+
+app.use(morgan('dev'));
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/p1', (_, res) => {
+  res.send('hello world!!').status(234);
+});
+
+app.get('/p2', (_, res) => {
+  res.send('hello world!!').status(234);
 });
 
 app.use((_, res) => {
-  res.status(404).json({ message: "This route not found" });
+  res.status(404).json({ message: 'This route not found' });
 });
 
-app.listen(PORT, (error) => {
+app.listen(PORT, error => {
   if (error) {
-    console.log("Server crushed. error: ", error);
+    console.log('Server crushed. error: ', error);
     process.exit(1);
   }
-  console.log("Server is running on port: ", PORT);
+  console.log('Server is running on port: ', PORT);
 });
