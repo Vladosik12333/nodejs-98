f06208b97e870450615fedd1dc86271da05f886e
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-05-19 13:35:09 +0300

    add not found middleware

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from "express";
-
+const PORT = 3000;
 const app = express();
 
-app.get("/p1", (req, res) => {
+app.get("/p1", (_, res) => {
   res.send("hello world!!").status(234);
 });
 
-app.listen(3000);
+app.use((_, res) => {
+  res.status(404).json({ message: "This route not found" });
+});
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log("Server crushed. error: ", error);
+    process.exit(1);
+  }
+  console.log("Server is running on port: ", PORT);
+});
